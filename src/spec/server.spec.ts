426249f607b2e6ceddc4f8d30905df719508a145
noplacetohide <nikhildwivedi007@gmail.com>
2024-05-14 07:11:10 +0530

    compile-contract-api

--- src/spec/server.spec.ts
+++ src/spec/server.spec.ts
@@ -2,10 +2,10 @@ import request from "supertest";
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import createServer from "../utils/server";
-import httpResponseMessages from "../constants/httpResponseMessages";
+import HTTP_RESPONSE_MESSAGES from "../constants/httpResponseMessages";
 export const app = createServer();
 
-describe(`APP should say ${httpResponseMessages.SERVER_UP_AND_RUNNING}`, () => {
+describe(`APP should say ${HTTP_RESPONSE_MESSAGES.SERVER_UP_AND_RUNNING}`, () => {
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
     await mongoose.connect(mongoServer.getUri());
@@ -18,7 +18,7 @@ describe(`APP should say ${httpResponseMessages.SERVER_UP_AND_RUNNING}`, () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject({
-      message: httpResponseMessages.SERVER_UP_AND_RUNNING,
+      message: HTTP_RESPONSE_MESSAGES.SERVER_UP_AND_RUNNING,
     });
     expect(response.body.success).toBeTruthy();
   });
