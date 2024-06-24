be11ea75cd471f778c7784c184673285525c89c4
noplacetohide <nikhildwivedi007@gmail.com>
2024-06-24 16:19:16 +0530

    Correction in Telegram Bot

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -49,7 +49,7 @@ export function createServer() {
 
     if (messageText === '/start') {
       // bot.sendMessage(chatId, 'Welcome to the bot!!');
-      bot.sendMessage(chatId, 'welcome to P-Tap. A product of Web3Agent. Click to play button to start game.')
+      bot.sendMessage(chatId, 'welcome to P-Tap. Click to play button to start game.')
     }
   });
   /** TELEGRAM POC ENDS HERE */
